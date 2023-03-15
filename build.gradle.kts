b336fc0bd1f3c90654be04b80a0150ff6c5c86de
Radinal Dwiki Novendra, M.T <112296875+23522046@users.noreply.github.com>
2023-03-15 14:41:42 +0700

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,39 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.8.10"
+    `maven-publish`
+}
+
+group = "org.example.detekt"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    compileOnly("io.gitlab.arturbosch.detekt:detekt-api:1.22.0")
+
+    testImplementation("io.gitlab.arturbosch.detekt:detekt-test:1.22.0")
+    testImplementation("io.kotest:kotest-assertions-core:5.5.5")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.9.2")
+}
+
+tasks.withType<KotlinCompile>() {
+    kotlinOptions.jvmTarget = "1.8"
+}
+
+tasks.withType<Test>().configureEach {
+    useJUnitPlatform()
+    systemProperty("junit.jupiter.testinstance.lifecycle.default", "per_class")
+    systemProperty("compile-snippet-tests", project.hasProperty("compile-test-snippets"))
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+        }
+    }
+}
